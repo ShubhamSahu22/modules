27590b078ca18640ca9fb9631fa24302e1b00b35
ShubhamSahu22 <shubhamsahu410@gmail.com>
2025-01-30 23:08:19 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "alb_dns_name" {
+  value       = module.alb.alb_dns_name
+  description = "DNS name of the Application Load Balancer"
+}
 
+output "autoscaling_group_name" {
+  value = module.autoscaling.autoscaling_group_name
+  description = "Name of the Auto Scaling Group"
+}
